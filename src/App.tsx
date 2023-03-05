9fc0eec2b3bb1309e963c420075a132b79cf0e86
Shannon Bentley <bentley.shannon@yahoo.com>
2023-03-05 14:06:23 -0600

    added routes to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 import './App.scss';
 
+import { Routes, Route } from 'react-router-dom';
+import { Container } from 'reactstrap';
+
 function App() {
   return (
-    <div>
-      HELLO WORLD
-    </div>
+    <Container>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='about' element={<About />} />
+        <Route path='shop' element={<Shop />} />
+        <Route path='contact' element={<Contact />} />
+      </Routes>
+    </Container>
   );
 }
 
