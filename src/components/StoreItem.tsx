f259b6abee6e9f28345d6f927f0b1f345c909090
Shannon Bentley <bentley.shannon@yahoo.com>
2023-03-05 17:12:26 -0600

    updated shop page with json items card component

---
+++ src/components/StoreItem.tsx
@@ -0,0 +1,23 @@
+import { Card, CardImg } from "reactstrap"
+
+type StoreItemProps = {
+    id: number
+    name: string
+    price: number
+    imgUrl: string
+}
+
+export const StoreItem = ({ id, name, price, imgUrl }: StoreItemProps) => {
+    return (
+        <Card>
+            <CardImg
+                variant='top'
+                src={imgUrl}
+                height='200px'
+                style={{
+                    objectFit: 'cover'
+                }}
+            />
+        </Card>
+    )
+}
--- src/pages/Shoppage.tsx
+++ src/pages/Shoppage.tsx
@@ -1,5 +1,20 @@
+import storeItems from '../data/items.json';
+import { StoreItem } from '../components/StoreItem';
+
+import { Row, Col } from 'reactstrap';
+
 export const Shoppage = () => {
     return (
-        <div>THIS IS THE SHOPPAGE</div>
+        <>
+            <div>STORE</div>
+
+            <Row md={2} xs={1} lg={3} className='g-3'>
+                {storeItems.map(item => (
+                    <Col>
+                        <StoreItem {...item} />
+                    </Col>
+                ))}
+            </Row>
+        </>
     )
 };
