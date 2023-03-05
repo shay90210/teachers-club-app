8e77a4365a3eebb754678f7206662bbcffdbab8e
Shannon Bentley <bentley.shannon@yahoo.com>
2023-03-05 14:02:28 -0600

    added browser router to index

--- src/index.tsx
+++ src/index.tsx
@@ -8,13 +8,16 @@ import 'bootstrap-social/bootstrap-social.css';
 import 'font-awesome/css/font-awesome.css';
 
 import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 
 const container = document.getElementById('root')!;
 const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
 
