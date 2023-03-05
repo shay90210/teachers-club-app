f25e81e65dce6dc39ae58b7b06ce35034f507ba4
Shannon Bentley <bentley.shannon@yahoo.com>
2023-03-05 13:54:31 -0600

    update

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import App from './App';
 
+import './App.scss';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-social/bootstrap-social.css';
 import 'font-awesome/css/font-awesome.css';
 
 import { createRoot } from 'react-dom/client';
-
-import './App.scss';
 
 const container = document.getElementById('root')!;
 const root = createRoot(container);
