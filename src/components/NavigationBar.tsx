b109fc255afd7aab5f71f88ec707dec7fcebeb88
Shannon Bentley <bentley.shannon@yahoo.com>
2023-03-05 14:53:57 -0600

    navigation bar

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -3,8 +3,7 @@ import {
     NavItem,
     Collapse,
     Navbar,
-    NavbarToggler,
-    Container
+    NavbarToggler
 }
     from 'reactstrap';
 import { NavLink } from 'react-router-dom';
@@ -20,27 +19,25 @@ export const NavigationBar = () => {
             className='main-nav'
             light
         >
-            <Container>
-                <NavbarToggler onClick={() => setMenuOpen(!menuOpen)} />
-                <Collapse isOpen={menuOpen} navbar>
-                    <Nav className='pt-3 pb-3' navbar>
-                        <div className='nav-links'>
-                            <NavItem>
-                                <NavLink className='nav-link' to='/'> HOME</NavLink>
-                            </NavItem>
-                            <NavItem>
-                                <NavLink className='nav-link' to='/about'> ABOUT</NavLink>
-                            </NavItem>
-                            <NavItem>
-                                <NavLink className='nav-link' to='/shop'> SHOP</NavLink>
-                            </NavItem>
-                            <NavItem>
-                                <NavLink className='nav-link' to='/contact'> CONTACT</NavLink>
-                            </NavItem>
-                        </div>
-                    </Nav>
-                </Collapse>
-            </Container>
+            <NavbarToggler onClick={() => setMenuOpen(!menuOpen)} />
+            <Collapse isOpen={menuOpen} navbar>
+                <Nav className='pt-3 pb-3' navbar>
+                    <div className='nav-links'>
+                        <NavItem>
+                            <NavLink className='nav-link' to='/'> HOME</NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink className='nav-link' to='/about'> ABOUT</NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink className='nav-link' to='/shop'> SHOP</NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink className='nav-link' to='/contact'> CONTACT</NavLink>
+                        </NavItem>
+                    </div>
+                </Nav>
+            </Collapse>
         </Navbar>
     )
 };
