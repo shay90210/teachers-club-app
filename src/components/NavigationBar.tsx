45f6aea88550b50a61bca7862fa4cfc388ee63de
Shannon Bentley <bentley.shannon@yahoo.com>
2023-03-05 15:34:43 -0600

    navigation bar

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -3,11 +3,17 @@ import {
     NavItem,
     Collapse,
     Navbar,
-    NavbarToggler
+    NavbarToggler,
+    Container,
+    Button
 }
     from 'reactstrap';
+
 import { NavLink } from 'react-router-dom';
 import { useState } from 'react';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faShoppingBag } from '@fortawesome/free-solid-svg-icons';
 
 export const NavigationBar = () => {
     const [menuOpen, setMenuOpen] = useState(false);
@@ -19,23 +25,26 @@ export const NavigationBar = () => {
             className='main-nav'
             light
         >
-            <NavbarToggler onClick={() => setMenuOpen(!menuOpen)} />
-            <Collapse isOpen={menuOpen} navbar>
-                <Nav className='ml-auto' align-items navbar>
-                    <NavItem>
-                        <NavLink className='nav-link' to='/'> HOME</NavLink>
-                    </NavItem>
-                    <NavItem>
-                        <NavLink className='nav-link' to='/about'> ABOUT</NavLink>
-                    </NavItem>
-                    <NavItem>
-                        <NavLink className='nav-link' to='/shop'> SHOP</NavLink>
-                    </NavItem>
-                    <NavItem>
-                        <NavLink className='nav-link' to='/contact'> CONTACT</NavLink>
-                    </NavItem>
-                </Nav>
-            </Collapse>
+            <Container>
+                <NavbarToggler onClick={() => setMenuOpen(!menuOpen)} />
+                <Collapse isOpen={menuOpen} navbar>
+                    <Nav className='me-auto' navbar>
+                        <NavItem>
+                            <NavLink className='nav-link' to='/'> HOME</NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink className='nav-link' to='/about'> ABOUT</NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink className='nav-link' to='/shop'> SHOP</NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink className='nav-link' to='/contact'> CONTACT</NavLink>
+                        </NavItem>
+                    </Nav>
+                    <Button><FontAwesomeIcon icon={faShoppingBag} /></Button>
+                </Collapse>
+            </Container>
         </Navbar>
     )
 };
