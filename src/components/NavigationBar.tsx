38f1e15fcdc89d5156608fbd21fd5fb9adc9832e
Shannon Bentley <bentley.shannon@yahoo.com>
2023-03-05 14:32:08 -0600

    created navigation bar

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -1,5 +1,46 @@
-export const NavigationBar = () => {
-    return (
-        <div>NAVBAR</div>
-    )
+import {
+    Nav,
+    NavItem,
+    Collapse,
+    Navbar,
+    NavbarToggler,
+    Container
 }
+    from 'reactstrap';
+import { NavLink } from 'react-router-dom';
+import { useState } from 'react';
+
+export const NavigationBar = () => {
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    return (
+        <Navbar
+            sticky='top'
+            expand='md'
+            className='main-nav'
+            light
+        >
+            <Container>
+                <NavbarToggler onClick={() => setMenuOpen(!menuOpen)} />
+                <Collapse isOpen={menuOpen} navbar>
+                    <Nav className='pt-3 pb-3' navbar>
+                        <div className='nav-links'>
+                            <NavItem>
+                                <NavLink className='nav-link' to='/'> HOME</NavLink>
+                            </NavItem>
+                            <NavItem>
+                                <NavLink className='nav-link' to='/about'> ABOUT</NavLink>
+                            </NavItem>
+                            <NavItem>
+                                <NavLink className='nav-link' to='/shop'> SHOP</NavLink>
+                            </NavItem>
+                            <NavItem>
+                                <NavLink className='nav-link' to='/contact'> CONTACT</NavLink>
+                            </NavItem>
+                        </div>
+                    </Nav>
+                </Collapse>
+            </Container>
+        </Navbar>
+    )
+};
