c41292446cedc95e55d1dec179f091ef04f183ec
Shannon Bentley <bentley.shannon@yahoo.com>
2023-03-05 14:29:48 -0600

    added navigation bar to app

--- src/App.tsx
+++ src/App.tsx
@@ -8,16 +8,21 @@ import { Aboutpage } from './pages/Aboutpage';
 import { Shoppage } from './pages/Shoppage';
 import { Contactpage } from './pages/Contactpage';
 
+import { NavigationBar } from './components/NavigationBar';
+
 function App() {
   return (
-    <Container className='mb-4'>
-      <Routes>
-        <Route path='/' element={<Homepage />} />
-        <Route path='/about' element={<Aboutpage />} />
-        <Route path='/shop' element={<Shoppage />} />
-        <Route path='/contact' element={<Contactpage />} />
-      </Routes>
-    </Container>
+    <>
+      <NavigationBar />
+      <Container className='mb-4'>
+        <Routes>
+          <Route path='/' element={<Homepage />} />
+          <Route path='/about' element={<Aboutpage />} />
+          <Route path='/shop' element={<Shoppage />} />
+          <Route path='/contact' element={<Contactpage />} />
+        </Routes>
+      </Container>
+    </>
   );
 }
 
