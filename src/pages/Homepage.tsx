581ca1dd82b69e88b56a3f87b37f78f6731d267c
Shannon Bentley <bentley.shannon@yahoo.com>
2023-03-06 11:25:49 -0600

    added photos to homepage

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -9,6 +9,41 @@ export const Homepage = () => {
                         <h2>The safe space to wear your thoughts!</h2>
                     </Col>
                 </Row>
+                <Row className='mt-5'>
+                    <Col>
+                        <img
+                            className='home-image-one'
+                            src='/img/mug3.jpg'
+                            alt='woman holding product'
+                            width='400px'
+                            style={{
+                                objectFit: 'cover'
+                            }}
+                        />
+                    </Col>
+                    <Col>
+                        <img
+                            className='home-image-two'
+                            src='/img/teacherhandbag.jpg'
+                            alt='man wearing handbag'
+                            width='400px'
+                            style={{
+                                objectFit: 'cover'
+                            }}
+                        />
+                    </Col>
+                    <Col>
+                        <img
+                            className='home-image-three'
+                            src='/img/beateacher.jpg'
+                            alt='woman wearing product'
+                            width='400px'
+                            style={{
+                                objectFit: 'cover'
+                            }}
+                        />
+                    </Col>
+                </Row>
             </Container>
         </div>
     )
