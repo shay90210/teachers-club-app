5ff3d1e04e8dd81c77fd6d9741296bfc774d768d
Shannon Bentley <bentley.shannon@yahoo.com>
2023-03-05 22:15:53 -0600

    created CartItem and ShoppingCart

--- src/components/ShoppingCart.tsx
+++ src/components/ShoppingCart.tsx
@@ -1,16 +1,28 @@
-import { OffcanvasTitle } from "react-bootstrap"
-import { Offcanvas, OffcanvasHeader } from "reactstrap"
+import Offcanvas from "react-bootstrap/Offcanvas";
+
+import { useShoppingCart } from "../context/ShoppingCartContext";
+import { Stack } from "react-bootstrap";
+
+import { CartItem } from "./CartItem";
 
 type ShoppingCartProps = {
     isOpen: boolean;
 }
 
 export const ShoppingCart = ({ isOpen }: ShoppingCartProps) => {
+    const { closeCart, cartItems } = useShoppingCart();
+
     return (
-        <Offcanvas>
-            <OffcanvasHeader close>
-                <OffcanvasTitle>CART SUMMARY</OffcanvasTitle>
-            </OffcanvasHeader>
+        <Offcanvas show={isOpen} onHide={closeCart}>
+            <Offcanvas.Header closeButton>
+                <Offcanvas.Title>CART SUMMARY</Offcanvas.Title>
+            </Offcanvas.Header>
+            <Offcanvas.Body>
+                <Stack gap={3}>
+                    {cartItems.map(item => (
+                        <CartItem key={item.id} {...item} />))}
+                </Stack>
+            </Offcanvas.Body>
         </Offcanvas>
     )
 }
