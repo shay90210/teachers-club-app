efbf5f46962534e81e23a04c5578048b516dc415
Shannon Bentley <bentley.shannon@yahoo.com>
2023-03-07 17:35:56 -0600

    updated contact page

--- src/pages/Contactpage.tsx
+++ src/pages/Contactpage.tsx
@@ -1,5 +1,10 @@
+import { ContactForm } from "../components/ContactForm";
+
 export const Contactpage = () => {
     return (
-        <div>THIS IS THE CONTACTPAGE</div>
+        <div>
+            <h3>Need Help? Contact us today!</h3>
+            <ContactForm />
+        </div>
     )
 };
