114411eb4479061d9e72548c93cd2a2dbaeb1a76
Shannon Bentley <bentley.shannon@yahoo.com>
2023-03-07 17:50:00 -0600

    updated and styled contact form

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -3,15 +3,18 @@ import {
     FormGroup,
     Label, Col,
     Input,
+    Button
 } from "reactstrap"
 
 export const ContactForm = () => {
+
     return (
-        <Form>
+        <Form className='contact-form'>
             <FormGroup row>
                 <Label
                     for='firstName'
                     sm={2}
+                    hidden
                 >
                     First Name
                 </Label>
@@ -28,6 +31,7 @@ export const ContactForm = () => {
                 <Label
                     for='lastName'
                     sm={2}
+                    hidden
                 >
                     Last Name
                 </Label>
@@ -44,6 +48,7 @@ export const ContactForm = () => {
                 <Label
                     for='userEmail'
                     sm={2}
+                    hidden
                 >
                     Email
                 </Label>
@@ -56,6 +61,90 @@ export const ContactForm = () => {
                     />
                 </Col>
             </FormGroup>
+            <FormGroup row>
+                <Label
+                    for='phoneNum'
+                    sm={2}
+                    hidden
+                >
+                    Phone Number
+                </Label>
+                <Col sm={10}>
+                    <Input
+                        id='phoneNum'
+                        name='number'
+                        placeholder='Phone Number'
+                        type='text'
+                    />
+                </Col>
+            </FormGroup>
+            <FormGroup row>
+                <Label
+                    for="contactCheckbox"
+                    sm={2}
+                >
+                    May we contact you?
+                </Label>
+                <Col
+                    sm={{
+                        size: 10
+                    }}
+                >
+                    <FormGroup check>
+                        <Input
+                            id="checkbox1"
+                            type="checkbox"
+                        />
+                        {' '}
+                        <Label check>
+                            Yes
+                        </Label>
+                    </FormGroup>
+                    <FormGroup check>
+                        <Input
+                            id="checkbox2"
+                            type="checkbox"
+                        />
+                        {' '}
+                        <Label check>
+                            No
+                        </Label>
+                    </FormGroup>
+                </Col>
+            </FormGroup>
+            <FormGroup
+                row
+                tag="fieldset"
+            >
+                <legend className="col-form-label col-sm-2">
+                    Contact Preference
+                </legend>
+                <Col sm={10}>
+                    <FormGroup check>
+                        <Input
+                            name="radio2"
+                            type="radio"
+                        />
+                        {' '}
+                        <Label check>
+                            Phone
+                        </Label>
+                    </FormGroup>
+                    <FormGroup check>
+                        <Input
+                            name="radio2"
+                            type="radio"
+                        />
+                        {' '}
+                        <Label check>
+                            Email
+                        </Label>
+                    </FormGroup>
+                </Col>
+            </FormGroup>
+            <Button className='form-button'>
+                Submit
+            </Button>
         </Form>
     )
 }
