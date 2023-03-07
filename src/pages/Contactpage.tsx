23b9162502903a198cc9e3b72cb263c4166af4e4
Shannon Bentley <bentley.shannon@yahoo.com>
2023-03-07 17:52:45 -0600

    updated contact page

--- src/pages/Contactpage.tsx
+++ src/pages/Contactpage.tsx
@@ -2,8 +2,12 @@ import { ContactForm } from "../components/ContactForm";
 
 export const Contactpage = () => {
     return (
-        <div>
-            <h3>Need Help? Contact us today!</h3>
+        <div className='contact-page'>
+            <h3>Contact Us</h3>
+            <p>
+                Need assistance or would like to give us a shout out? Submit the form, and we
+                will contact you within the next 48-72 hours.
+            </p>
             <ContactForm />
         </div>
     )
