0e2509f6796cd6a9cd75757b6466fd7d4545c84a
Shannon Bentley <bentley.shannon@yahoo.com>
2023-03-08 12:57:31 -0600

    updated and styled about page

--- src/pages/Aboutpage.tsx
+++ src/pages/Aboutpage.tsx
@@ -1,5 +1,26 @@
+import { Row, Col, Container } from "reactstrap";
+
 export const Aboutpage = () => {
     return (
-        <div>THIS IS THE ABOUTPAGE</div>
+        <div className='about-page'>
+            <h2 className='about-title'>ABOUT THE TEACHERS' CLUB</h2>
+            <Container>
+                <Row>
+                    <Col>
+                        <p>
+                            As educators, we have the privilege of being in a signature, exemplary club that
+                            is only understood by fellow teachers. While navigating the world of education
+                            can be difficult, all of the long hours, grading, and lesson planning is worth
+                            the impact we have on the future leaders of this world.
+                        </p>
+                        <p>The sole purpose of The Teachers' Club is to provide educators with apparel
+                            and merchandise that evoke the common feelings we all share, while celebrating
+                            the wonderful work we're doing each day. We hope you find something in The
+                            Teachers' Club that you feel proud to wear out loud.
+                        </p>
+                    </Col>
+                </Row>
+            </Container>
+        </div>
     )
 };
