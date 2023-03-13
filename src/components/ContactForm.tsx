530d93f4ca8ca2b2cc4e608ca8346dcbb2b9ee8b
Shannon Bentley <bentley.shannon@yahoo.com>
2023-03-12 22:53:14 -0500

    updated Formik

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -5,159 +5,170 @@ import {
     Input,
     Button
 } from "reactstrap"
+import { Formik } from "formik"
 
 export const ContactForm = () => {
 
     return (
-        <Form className='contact-form'>
-            <FormGroup row>
-                <Label
-                    for='firstName'
-                    sm={2}
-                >
-                    First Name
-                </Label>
-                <Col sm={10}>
-                    <Input
-                        id='firstName'
-                        name='firstname'
-                        placeholder='First Name'
-                        type='text'
-                    />
-                </Col>
-            </FormGroup>
-            <FormGroup row>
-                <Label
-                    for='lastName'
-                    sm={2}
-                >
-                    Last Name
-                </Label>
-                <Col sm={10}>
-                    <Input
-                        id='lastName'
-                        name='lastname'
-                        placeholder='Last Name'
-                        type='text'
-                    />
-                </Col>
-            </FormGroup>
-            <FormGroup row>
-                <Label
-                    for='userEmail'
-                    sm={2}
-                >
-                    Email
-                </Label>
-                <Col sm={10}>
-                    <Input
-                        id='userEmail'
-                        name='emailAddress'
-                        placeholder='Email'
-                        type='email'
-                    />
-                </Col>
-            </FormGroup>
-            <FormGroup row>
-                <Label
-                    for='phoneNum'
-                    sm={2}
-                >
-                    Phone Number
-                </Label>
-                <Col sm={10}>
-                    <Input
-                        id='phoneNum'
-                        name='number'
-                        placeholder='Phone Number'
-                        type='text'
-                    />
-                </Col>
-            </FormGroup>
-            <FormGroup row>
-                <Label
-                    for="contactCheckbox"
-                    sm={2}
-                    className='form-label'
-                >
-                    May we contact you?
-                </Label>
-                <Col
-                    sm={{
-                        size: 10
-                    }}
-                >
-                    <FormGroup check inline>
+        <Formik
+            initialValues={ }
+            onSubmit={(values, { setSubmitting }) => {
+                setTimeout(() => {
+                    alert(JSON.stringify(values, null, 2));
+                    setSubmitting(false);
+                }, 400);
+            }}
+        >
+            <Form className='contact-form'>
+                <FormGroup row>
+                    <Label
+                        for='firstName'
+                        sm={2}
+                    >
+                        First Name
+                    </Label>
+                    <Col sm={10}>
                         <Input
-                            id="checkbox1"
-                            type="checkbox"
+                            id='firstName'
+                            name='firstname'
+                            placeholder='First Name'
+                            type='text'
                         />
-                        {' '}
-                        <Label check>
-                            Yes
-                        </Label>
-                    </FormGroup>
-                    <FormGroup check inline>
+                    </Col>
+                </FormGroup>
+                <FormGroup row>
+                    <Label
+                        for='lastName'
+                        sm={2}
+                    >
+                        Last Name
+                    </Label>
+                    <Col sm={10}>
                         <Input
-                            id="checkbox2"
-                            type="checkbox"
+                            id='lastName'
+                            name='lastname'
+                            placeholder='Last Name'
+                            type='text'
                         />
-                        {' '}
-                        <Label check>
-                            No
-                        </Label>
-                    </FormGroup>
-                </Col>
-            </FormGroup>
-            <FormGroup
-                row
-                tag="fieldset"
-            >
-                <legend className="col-form-label col-sm-2">
-                    Contact Preference
-                </legend>
-                <Col sm={10}>
-                    <FormGroup check inline>
+                    </Col>
+                </FormGroup>
+                <FormGroup row>
+                    <Label
+                        for='userEmail'
+                        sm={2}
+                    >
+                        Email
+                    </Label>
+                    <Col sm={10}>
                         <Input
-                            name="radio2"
-                            type="radio"
+                            id='userEmail'
+                            name='emailAddress'
+                            placeholder='Email'
+                            type='email'
                         />
-                        {' '}
-                        <Label check>
-                            Phone
-                        </Label>
-                    </FormGroup>
-                    <FormGroup check inline>
+                    </Col>
+                </FormGroup>
+                <FormGroup row>
+                    <Label
+                        for='phoneNum'
+                        sm={2}
+                    >
+                        Phone Number
+                    </Label>
+                    <Col sm={10}>
                         <Input
-                            name="radio2"
-                            type="radio"
+                            id='phoneNum'
+                            name='number'
+                            placeholder='Phone Number'
+                            type='text'
                         />
-                        {' '}
-                        <Label check>
-                            Email
-                        </Label>
-                    </FormGroup>
-                </Col>
-            </FormGroup>
-            <FormGroup row>
-                <Label
-                    for="userText"
-                    sm={2}
-                    className='form-label'
+                    </Col>
+                </FormGroup>
+                <FormGroup row>
+                    <Label
+                        for="contactCheckbox"
+                        sm={2}
+                        className='form-label'
+                    >
+                        May we contact you?
+                    </Label>
+                    <Col
+                        sm={{
+                            size: 10
+                        }}
+                    >
+                        <FormGroup check inline>
+                            <Input
+                                id="checkbox1"
+                                type="checkbox"
+                            />
+                            {' '}
+                            <Label check>
+                                Yes
+                            </Label>
+                        </FormGroup>
+                        <FormGroup check inline>
+                            <Input
+                                id="checkbox2"
+                                type="checkbox"
+                            />
+                            {' '}
+                            <Label check>
+                                No
+                            </Label>
+                        </FormGroup>
+                    </Col>
+                </FormGroup>
+                <FormGroup
+                    row
+                    tag="fieldset"
                 >
-                    Message
-                </Label>
-                <Col sm={10}>
-                    <Input
-                        id="useText"
-                        name="text"
-                        type="textarea"
-                    />
-                </Col>
-            </FormGroup>
-            <Button className='form-button'>
-                Submit
-            </Button>
-        </Form>
+                    <legend className="col-form-label col-sm-2">
+                        Contact Preference
+                    </legend>
+                    <Col sm={10}>
+                        <FormGroup check inline>
+                            <Input
+                                name="radio2"
+                                type="radio"
+                            />
+                            {' '}
+                            <Label check>
+                                Phone
+                            </Label>
+                        </FormGroup>
+                        <FormGroup check inline>
+                            <Input
+                                name="radio2"
+                                type="radio"
+                            />
+                            {' '}
+                            <Label check>
+                                Email
+                            </Label>
+                        </FormGroup>
+                    </Col>
+                </FormGroup>
+                <FormGroup row>
+                    <Label
+                        for="userText"
+                        sm={2}
+                        className='form-label'
+                    >
+                        Message
+                    </Label>
+                    <Col sm={10}>
+                        <Input
+                            id="useText"
+                            name="text"
+                            type="textarea"
+                        />
+                    </Col>
+                </FormGroup>
+                <Button className='form-button'>
+                    Submit
+                </Button>
+            </Form>
+        </Formik>
     )
 }
